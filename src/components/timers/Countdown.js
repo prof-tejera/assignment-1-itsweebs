399ef78f1a5dfab07e0d4b1742590f2ba23c869e
itsweebs <90012218+itsweebs@users.noreply.github.com>
2023-11-01 08:00:46 -0400

    created stopwatch timer

--- src/components/timers/Countdown.js
+++ src/components/timers/Countdown.js
@@ -3,67 +3,67 @@ import Panel from "../generic/Panel.js";
 import Button from "../generic/Button.js";
 import DisplayTime from "../generic/DisplayTime.js";
 
-const CountdownTimer = () => {
+const Countdown = () => {
     //state to keep track of time
     const [time, setTime] = useState(150); // 2m 30s
     //state to determine if timer is running
     const [isRunning, setIsRunning] = useState(false);
 
-     //handle countdown logic
+    //handle countdown logic
     useEffect(() => {
-    let interval;
+        let interval;
 
-    //if timer is running and time is greater than 0, start countdown
-    if (isRunning && time > 0) {
-        interval = setInterval(() => {
-            setTime((prevTime) => prevTime - 1); // Decrease time by 1 every second
-        }, 1000);
-    } else if (!isRunning || time === 0) {
-        // If timer is not running or time is 0, clear interval
-        clearInterval(interval);
-    }
+        //if timer is running and time is greater than 0, start countdown
+        if (isRunning && time > 0) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime - 1); //decrease time by 1 every second
+            }, 1000);
+        } else if (!isRunning || time === 0) {
+            // If timer is not running or time is 0, clear interval
+            clearInterval(interval);
+        }
 
-    //clear interval when component unmounts or when it's running
-    return () => clearInterval(interval);
-}, [isRunning, time]);
+        //clear interval when component unmounts or when it's running
+        return () => clearInterval(interval);
+    }, [isRunning, time]);
 
-//format time in minutes and seconds
-const formatTime = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    //format time in minutes and seconds
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
+    //function to start or pause the timer
+    const startPauseTimer = () => {
+        setIsRunning(!isRunning);
+    };
+
+    //function to reset the timer
+    const resetTimer = () => {
+        setIsRunning(false);
+        setTime(150);
+    };
+
+    //function to end the timer
+    const fastForwardTimer = () => {
+        setTime(0);
+        setIsRunning(false);
+    };
+
+    //render the countdown timer and control buttons
+    return (
+        <div>
+            <DisplayTime>
+                <span>{formatTime(time)}</span>
+            </DisplayTime>
+            <Panel>
+                <Button label={isRunning ? 'Pause' : 'Start'} onClick={startPauseTimer} />
+                <Button label="Reset" onClick={resetTimer} />
+                <Button label="Fast Forward" onClick={fastForwardTimer} />
+            </Panel>
+        </div>
+    );
 };
 
-//function to start or pause the timer
-const startPauseTimer = () => {
-    setIsRunning(!isRunning);
-};
-
-//function to reset the timer
-const resetTimer = () => {
-    setIsRunning(false);
-    setTime(150);
-};
-
-//function to end the timer
-const fastForwardTimer = () => {
-    setTime(0);
-    setIsRunning(false);
-};
-
-// Render the countdown timer and control buttons
-return (
-    <div>
-    <DisplayTime>
-        <span>{formatTime(time)}</span>
-      </DisplayTime>
-    <Panel>
-        <Button label={isRunning ? 'Pause' : 'Start'} onClick={startPauseTimer} />
-        <Button label="Reset" onClick={resetTimer} />
-        <Button label="Fast Forward" onClick={fastForwardTimer} />
-    </Panel>
-    </div>
-);
-};  
-
-export default CountdownTimer;
+export default Countdown;
--- src/components/timers/Stopwatch.js
+++ src/components/timers/Stopwatch.js
@@ -1,3 +1,69 @@
-const Stopwatch = () => null;
+import { useState, useEffect } from "react";
+import Panel from "../generic/Panel.js";
+import Button from "../generic/Button.js";
+import DisplayTime from "../generic/DisplayTime.js";
+
+const Stopwatch = () => {
+    //state to keep track of time
+    const [time, setTime] = useState(0);
+    //state to determine if timer is running
+    const [isRunning, setIsRunning] = useState(false);
+
+    //handle countdown logic
+    useEffect(() => {
+        let interval;
+
+        //if timer is running and time is less than 2m 30s, start stopwatch
+        if (isRunning && time < 150) {
+            interval = setInterval(() => {
+                setTime((prevTime) => prevTime + 1); //increase time by 1 every second
+            }, 1000);
+        } else if (!isRunning || time === 150) {
+            //if timer is not running or time is 2m 30s, clear interval
+            clearInterval(interval);
+        }
+
+        //clear interval when component unmounts or when it's not running
+        return () => clearInterval(interval);
+    }, [isRunning, time]);
+
+    //format time in minutes and seconds
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    };
+
+    //function to start or pause the timer
+    const startPauseTimer = () => {
+        setIsRunning(!isRunning);
+    };
+
+    //function to reset the timer
+    const resetTimer = () => {
+        setIsRunning(false);
+        setTime(0);
+    };
+
+    //function to end the timer
+    const fastForwardTimer = () => {
+        setTime(150);
+        setIsRunning(false);
+    };
+
+    //render the stopwatch and control buttons
+    return (
+        <div>
+            <DisplayTime>
+                <span>{formatTime(time)}</span>
+            </DisplayTime>
+            <Panel>
+                <Button label={isRunning ? "Pause" : "Start"} onClick={startPauseTimer} />
+                <Button label="Reset" onClick={resetTimer} />
+                <Button label="Fast Forward" onClick={fastForwardTimer} />
+            </Panel>
+        </div>
+    );
+};
 
 export default Stopwatch;
