934af9e39dc6a1502d4b8bb1b2dd4a720dc5f8ce
itsweebs <90012218+itsweebs@users.noreply.github.com>
2023-11-02 17:28:07 -0400

    add documentation for generic components

--- src/views/DocumentationView.js
+++ src/views/DocumentationView.js
@@ -4,6 +4,14 @@ import styled from "styled-components";
 import DocumentComponent from "../components/documentation/DocumentComponent";
 
 import Loading from "../components/generic/Loading";
+import Panel from "../components/generic/Panel";
+import Button from "../components/generic/Button";
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import DisplayTime from "../components/generic/DisplayTime";
+import DisplayRounds from "../components/generic/DisplayRounds";
+import Input from "../components/generic/Input";
+
+
 
 const Container = styled.div`
   display: flex;
@@ -35,8 +43,100 @@ const Documentation = () => {
             },
           ]}
         />
+        <DocumentComponent
+          title="Panel"
+          component={<Panel className="control-panel">Sample Content</Panel>}
+          propDocs={[
+            {
+              prop: "children",
+              description: "Contents contained inside the panel",
+              type: "node",
+              defaultValue: "n/a",
+            },
+            {
+              prop: "className",
+              description: "Assigns a class name for additional styling purposes",
+              type: "string",
+              defaultValue: "none",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="Button"
+          component={<Button label="Sample Button" icon={faPlay} onClick={() => console.log('Button clicked!')} />}
+          propDocs={[
+            {
+              prop: "label",
+              description: "Label for the button if no icon is provided",
+              type: "string",
+              defaultValue: "none",
+            },
+            {
+              prop: "icon",
+              description: "Icon to be displayed on the button",
+              type: "object",
+              defaultValue: "none",
+            },
+            {
+              prop: "className",
+              description: "Assigns a class name for additional styling purposes",
+              type: "string",
+              defaultValue: "none",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="Display Time"
+          component={<DisplayTime>01:30</DisplayTime>}
+          propDocs={[
+            {
+              prop: "children",
+              description: "Contents contained inside Display Time",
+              type: "node",
+              defaultValue: "n/a",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="Display Rounds"
+          component={<DisplayRounds text="Round 1 of 2" />}
+          propDocs={[
+            {
+              prop: "text",
+              description: "Displays the tracked rounds",
+              type: "string",
+              defaultValue: "none",
+            },
+          ]}
+        />
+        <DocumentComponent
+          title="Input"
+          component={<Input label="Sample Label" />}
+          propDocs={[
+            {
+              prop: "label",
+              description: "Label text for the input field",
+              type: "string",
+              defaultValue: "n/a",
+            },
+            {
+              prop: "pattern",
+              description: "Pattern to validate the input",
+              type: "string",
+              defaultValue: "MM:SS",
+            },
+            {
+              prop: "maxLength",
+              description: "Maximum allowed length for the input",
+              type: "number",
+              defaultValue: "5",
+            },
+          ]}
+        />
       </div>
     </Container>
+
+
   );
 };
 
